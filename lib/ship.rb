9fb1e2aaf7b577f5016c5922b184383bf947b6af
Dani Coleman <danicoleman00@gmail.com>
2020-07-06 18:24:58 -0600

    Remove/add spacing

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,3 @@
-# Ship
-# A Ship object will represent a single ship on the board.
-# It will be able to keep track of how much health it has, take hits,
-# and report if it is sunk or not. A ship should start off with health equal
-# to it’s length.
-
 class Ship
 
   attr_reader :name,
@@ -22,5 +16,4 @@ class Ship
   def hit
     @health -= 1
   end
-
 end
