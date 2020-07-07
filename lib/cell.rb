e856b17f5e6e109c24f8773de4b84a768c2863eb
Dani Coleman <danicoleman00@gmail.com>
2020-07-06 18:59:33 -0600

    Create cell class and first 2 method patterns

---
+++ lib/cell.rb
@@ -0,0 +1,32 @@
+class Cell
+  attr_reader :coordinate,
+              :ship,
+              :fired_upon
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+    @ship = nil
+    @fired_upon = false
+  end
+
+  def empty?
+    if @ship == nil
+      return true
+    else
+      return false
+    end
+  end
+
+  def place_ship(ship_to_place)
+    @ship = ship_to_place
+  end
+
+  def fire_upon
+    ship.hit if @ship
+    @fired_upon = true
+  end
+
+  def fired_upon?
+    @fired_upon
+  end
+end
