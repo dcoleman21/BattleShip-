9e8475b3aa594169dcd7832d1222a5ee4b76c8e0
Dani Coleman <danicoleman00@gmail.com>
2020-07-07 17:39:48 -0600

    Complete iteration 1 methods

--- lib/cell.rb
+++ lib/cell.rb
@@ -29,4 +29,18 @@ class Cell
   def fired_upon?
     @fired_upon
   end
+
+  def render(show_ship = false)
+    if fired_upon? == false && ship && show_ship == true
+      return "S"
+    elsif ship && fired_upon? == true && ship.sunk?
+      return "X"
+    elsif ship.nil? && fired_upon? == true
+      return "M"
+    elsif ship && fired_upon? == true
+      return "H"
+    else 
+      return "."
+     end
+   end
 end
