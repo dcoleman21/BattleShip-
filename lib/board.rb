b6e1d4468b2fa67740ee1b03f88c54ada17d329f
fieldstyler <tdfields93@gmail.com>
2020-07-09 15:43:33 -0600

    Added helper methods for valid placement.

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Board
 
   attr_reader :cells
@@ -22,18 +23,61 @@ class Board
     }
   end
 
-  def valid_coordinate?(coordinate)
-    cells.keys.include?(coordinate)
-    #   return true
-    # else
-    #   return false
-    # end ======> works but redundant
-  end
 
   def valid_placement?(ship_object, coordinates)
-    ship_object.length == coordinates.size && valid_coordinate? == true
-    #
-    #coordinates are consectutive
-    #
+    valid_placement_length?(ship_object, coordinates) &&
+    consecutive_placements?(coordinates) ||
+    valid_coordinate?(coordinates)
+  # return true if
+    # true
   end
+
+  def valid_coordinate?(coordinate)
+    @cells.keys.include?(coordinate)
+  end
+
+  def valid_placement_length?(ship_object, coordinates)
+    ship_object.length == coordinates.size
+  end
+
+  def consecutive_placements?(coordinates)
+    letters = coordinates.map do |coordinate|
+      coordinate[0].ord
+    end
+    numbers = coordinates.map do |coordinate|
+      coordinate[-1].to_i
+    end
+    if letters.each_cons(2).map { |a,b| b - a }.all? { |letter| letter == 1 } && numbers.uniq.count == 1
+      true
+    elsif numbers.each_cons(2).map { |a,b| b - a }.all? { |number| number == 1 } && letters.uniq.count == 1
+      true
+    else
+      false
+    end
+  end
+
+  def diagonal_coordinates?(coordinates)
+    letters = coordinates.map do |coordinate|
+      coordinate[0].ord
+    end
+    numbers = coordinates.map do |coordinate|
+      coordinate[-1].to_i
+    end
+
+    letters.uniq.count == 1 || numbers.uniq.count == 1
+  end
+
 end
+
+
+# def consecutive_coordinates?(coordinates)
+#   if same_number?(coordinates) == true && same_letter?(coordinates) == false
+#     true
+#   elsif same_number?(coordinates) == false && same_letter?(coordinates) == true
+#     true
+#   elsif same_number?(coordinates) && same_letter?(coordinates)
+#     false
+#   else
+#     false
+#   end
+# end
