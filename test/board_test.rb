595dee512656579d9ac43ad1602279f55068593f
Dani Coleman <danicoleman00@gmail.com>
2020-07-09 18:16:44 -0600

    Add test for place ship and overlap

--- test/board_test.rb
+++ test/board_test.rb
@@ -98,7 +98,37 @@ class BoardTest < Minitest::Test
 
     assert_equal true, board.consecutive_placements?(["A1", "A2", "A3"])
     assert_equal false, board.consecutive_placements?(["A1", "C1"])
-
   end
+
+  def test_can_place_ships
+    board = Board.new
+    board.cells
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    cell_1 = board.cells["A1"]
+    cell_2 = board.cells["A2"]
+    cell_3 = board.cells["A3"]
+
+    assert_equal cruiser, cell_1.ship
+    assert_equal cruiser, cell_2.ship
+    assert_equal cruiser, cell_3.ship
+
+    assert_equal true, cell_3.ship == cell_2.ship
+  end
+
+  def test_ships_dont_overlap
+    board = Board.new
+    board.cells
+    cruiser = Ship.new("Cruiser", 3)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false, board.valid_placement?(submarine, ["B1", "B2"])
+  end
+
 
 end
