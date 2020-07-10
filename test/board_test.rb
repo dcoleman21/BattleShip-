ba80cc941fb1decd9788d1d9a4764968d9df28cb
Dani Coleman <danicoleman00@gmail.com>
2020-07-10 14:32:13 -0600

    Remove extra spaces and rename tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -4,18 +4,19 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require "pry"
+
 class BoardTest < Minitest::Test
 
-  def test_board_exists
+  def test_it_exists
     board = Board.new
     assert_instance_of Board, board
   end
 
-  def test_board_has_cells
+  def test_it_has_cells
     board = Board.new
     assert_equal 16, board.cells.count
-    assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1",
-      "C2", "C3", "C4", "D1", "D2", "D3", "D4"], board.cells.keys
+    assert_equal ["A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4", "C1", "C2",
+    "C3", "C4", "D1", "D2", "D3", "D4"], board.cells.keys
     assert_equal Hash, board.cells.class
     assert_equal Cell, board.cells.values[0].class
     assert_equal Cell, board.cells.values[1].class
@@ -35,22 +36,26 @@ class BoardTest < Minitest::Test
     assert_equal Cell, board.cells.values[15].class
   end
 
-  def test_board_has_valid_coordinates
+  def test_there_are_valid_coordinates
     board = Board.new
     board.cells
+
     assert_equal true, board.valid_coordinate?("A1")
     assert_equal true, board.valid_coordinate?("D4")
+
     assert_equal false, board.valid_coordinate?("A5")
     assert_equal false, board.valid_coordinate?("E1")
     assert_equal false, board.valid_coordinate?("A22")
+
     assert_equal true, board.valid_coordinate?("A2")
   end
 
-  def test_board_has_ship_length_matches_coordinate_size
+  def test_ship_length_matches_coordinate_size
     board = Board.new
     board.cells
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal false, board.valid_placement_length?(cruiser, ["A1", "A2"])
     assert_equal false, board.valid_placement_length?(submarine, ["A2", "A3", "A4"])
   end
@@ -60,10 +65,14 @@ class BoardTest < Minitest::Test
     board.cells
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
     assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
     assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
     assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 
   def test_coordinates_are_diagonal
@@ -71,29 +80,22 @@ class BoardTest < Minitest::Test
     board.cells
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal true, board.diagonal_coordinates?(cruiser, ["A1", "B2", "C3"])
     assert_equal true, board.diagonal_coordinates?(submarine, ["C2", "D3"])
   end
 
-  def test_coordinates_invalid_if_diagonal
+  def test_valid_placement_invalid_if_diagonal
     board = Board.new
     board.cells
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
     assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
   end
 
-  def test_coordinates_are_validly_placed
-    board = Board.new
-    board.cells
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
-    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
-  end
-
-  def test_has_consecutive_placements
+  def test_coordinates_have_consecutive_placements
     board = Board.new
     board.cells
     cruiser = Ship.new("Cruiser", 3)
@@ -138,10 +140,14 @@ class BoardTest < Minitest::Test
     board.cells
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
+
     assert_equal false, board.contains_ship?(submarine, ["A1", "A2"])
+
     board.place(cruiser, ["A1", "A2", "A3"])
+
     assert_equal true, board.contains_ship?(cruiser, ["A1", "A2", "A3"])
     assert_equal true, board.contains_ship?(cruiser, ["A1", "B1", "C1"])
+
     assert_equal false, board.contains_ship?(cruiser, ["B1", "C1", "D1"])
   end
 
@@ -154,7 +160,7 @@ class BoardTest < Minitest::Test
     board.place(cruiser, ["A1", "A2", "A3"])
 
     assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", board.render
-
+    
     assert_equal "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n", board.render(true)
   end
 end
