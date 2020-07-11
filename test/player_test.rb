a8dfec46af196aafa78e5525b26575eb9e20822d
Dani Coleman <danicoleman00@gmail.com>
2020-07-11 08:50:00 -0600

    Create player class tests

--- test/player_test.rb
+++ test/player_test.rb
@@ -3,13 +3,36 @@ require 'minitest/pride'
 require './lib/board'
 require './lib/cell'
 require './lib/ship'
+require './lib/player'
 
 class PlayerTest < Minitest::Test
 
   def test_it_exists
-    computer = Player.new
-    player = Player.new
-    board = board.new
+    computer = Player.new("Computer")
+    human = Player.new("Human")
+
+    assert_instance_of Player, computer
+    assert_instance_of Player, human
+  end
+
+  def test_it_has_a_player_type
+    computer = Player.new("Computer")
+    human = Player.new("Human")
+
+    assert_equal "Computer", computer.type
+    assert_equal "Human", human.type
+  end
+
+  def test_it_has_a_board
+    computer = Player.new("Computer")
+    human = Player.new("Human")
+
+    expected = ["A1", "A2", "A3", "A4", "B1", "B2",
+      "B3", "B4", "C1", "C2","C3", "C4", "D1", "D2",
+      "D3", "D4"]
+
+    assert_equal expected, computer.board.cells.keys
+    assert_equal expected, human.board.cells.keys
   end
 
 end
