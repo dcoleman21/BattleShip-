28ddfa3aa065ebfc34014f1ebbae55820af6cb2b
Dani Coleman <danicoleman00@gmail.com>
2020-07-11 09:49:08 -0600

    Test that each player can have ships and place them

--- test/player_test.rb
+++ test/player_test.rb
@@ -35,4 +35,26 @@ class PlayerTest < Minitest::Test
     assert_equal expected, human.board.cells.keys
   end
 
+  def test_computer_has_ships
+    computer = Player.new("Computer")
+
+    assert_instance_of Ship, computer.cruiser
+    assert_instance_of Ship, computer.submarine
+  end
+
+  def test_computer_can_place_cruiser
+    computer = Player.new("Computer")
+    computer.place_cruiser
+
+    assert_equal [@cell, @cell, @cell], computer.board.place #saying it wants args
+  end
+
+  def test_human_has_ship
+    human = Player.new("Human")
+
+    assert_instance_of Ship, human.cruiser
+    assert_instance_of Ship, human.submarine
+  end
+
+
 end
