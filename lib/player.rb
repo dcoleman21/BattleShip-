53e9b700241428c28dc3aa692fef824877db42da
Dani Coleman <danicoleman00@gmail.com>
2020-07-11 09:48:36 -0600

    Figure out settting ships for each player

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,27 @@
 class Player
   attr_reader :board,
-              :type
+              :type,
+              :cruiser,
+              :submarine
 
   def initialize(player_type)
-    @type  = player_type
-    @board = Board.new
+    @type      = player_type
+    @board     = Board.new
+    @cruiser   = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
+
+  # def place_cruiser
+  #   loop do
+  #     computer_cruiser_coords = Array.new
+  #     until computer_cruiser_coords.length == cruiser.length do
+  #       computer_cruiser_coords << board.cells.keys.sample
+  #     end
+  #     if board.valid_placement?(cruiser, computer_cruiser_coords)
+  #       @board.place(cruiser, computer_cruiser_coords)
+  #       break
+  #     end
+  #   end
+  # end
 
 end
