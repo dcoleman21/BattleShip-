78bc4f373a833761a1c12e5b13a321f61da497c7
Dani Coleman <danicoleman00@gmail.com>
2020-07-10 18:13:50 -0600

    Create player test

---
+++ test/player_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
+class PlayerTest < Minitest::Test
+
+  def test_it_exists
+    computer = Player.new
+    player = Player.new
+    board = board.new
+  end
+
+end
