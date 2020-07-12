a7f1cd7fb5cb35b3f9e0505e4266bdb2f7f8ad2b
Dani Coleman <danicoleman00@gmail.com>
2020-07-12 10:25:05 -0600

    Finalize player class

--- lib/player.rb
+++ lib/player.rb
@@ -10,18 +10,4 @@ class Player
     @cruiser   = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
   end
-
-  # def place_cruiser
-  #   loop do
-  #     computer_cruiser_coords = Array.new
-  #     until computer_cruiser_coords.length == cruiser.length do
-  #       computer_cruiser_coords << board.cells.keys.sample
-  #     end
-  #     if board.valid_placement?(cruiser, computer_cruiser_coords)
-  #       @board.place(cruiser, computer_cruiser_coords)
-  #       break
-  #     end
-  #   end
-  # end
-
 end
