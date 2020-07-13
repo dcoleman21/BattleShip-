347c783ec2db1e0053f37beb763f80ae2693bd9c
Dani Coleman <danicoleman00@gmail.com>
2020-07-13 09:04:04 -0600

    Work to figure out failure

--- test/game_test.rb
+++ test/game_test.rb
@@ -6,3 +6,15 @@ require './lib/game'
 require './lib/turn'
 require './lib/player'
 require './lib/board'
+
+class GameTest < Minitest::Test
+
+  def test_it_exists
+    game = Game.new.start
+    computer = Board.new
+    human = Board.new
+
+    assert_instance_of Game, game
+  end
+
+end
